89dd5d4dd5615794801ffadf056a77dc281bd1b8
Cristian Consonni <cristian@balist.es>
2018-09-01 23:28:08 +0200

    Add testjob_countdown.sh

---
+++ cluster/testjob_countdown.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#PBS -V
+#PBS -l walltime=00:02:00
+#PBS -l select=1:ncpus=1
+#PBS -q cpuq
+
+#shellcheck disable=SC2128
+SOURCED=false && [ "$0" = "$BASH_SOURCE" ] || SOURCED=true
+if ! $SOURCED; then
+  set -euo pipefail
+  IFS=$'\n\t'
+fi
+
+i="$1"
+
+echo "Testing $0 ($2): countdown for $i seconds ..."
+while ((i>0)); do
+  echo "$i"
+  sleep 1
+  i=$((i-1))
+done
+echo "Done!"
+
+exit 0
+
