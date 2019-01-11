1ccbb6e3499db5d4cd876781ec02e472a0f221b3
Cristian Consonni <cristian@balist.es>
2019-01-11 20:30:55 +0100

    Several updates

--- merge/merge_linkextractions.sh
+++ merge/merge_linkextractions.sh
@@ -12,7 +12,7 @@ verbose=false
 input=''
 
 eval "$(docopts -V - -h - : "$@" <<EOF
-Usage: merge_snapshots.sh [options] --input INPUT_FILE DATE
+Usage: merge_linkextractions.sh [options] --input INPUT_FILE DATE
 
       DATE                                  Date to merge
       --input INPUT_FILE                    Input file with list
@@ -24,8 +24,8 @@ Usage: merge_snapshots.sh [options] --input INPUT_FILE DATE
       --version                             Print version and copyright
                                             information.
 ----
-merge_snapshots.sh 0.1.0
-copyright (c) 2016 Cristian Consonni
+merge_snapshots.sh 0.2.0
+copyright (c) 2018 Cristian Consonni
 MIT License
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
@@ -33,39 +33,47 @@ EOF
 )"
 
 case "$output_compression" in
-
-    "gzip")
-        compression_command='gzip'
-        ;;
-    "7z")
-        compression_command='7z a -si'
-        ;;
-    *)
-        unset compression_commnad
-        ;;
+  "gzip")
+      compression_command='gzip'
+      ;;
+  "7z")
+      compression_command='7z a -si'
+      ;;
+  *)
+      unset compression_commnad
+      ;;
 esac
 
 if $verbose; then
-    echo "date: $DATE"
-    echo "input: $input"
+  echo "date: $DATE"
+  echo "input: $input"
 
-    if [[ -n "$compression_command" ]]; then
-        echo -n "output compression: $output_compression"
-        echo    " - compression commnand: $compression_command"
-    else
-        echo "ouput compression: None"
-    fi
+  if [[ -n "$compression_command" ]]; then
+      echo -n "output compression: $output_compression"
+      echo    " - compression commnand: $compression_command"
+  else
+      echo "ouput compression: None"
+  fi
 fi
 
 echo "$DATE -> links_snapshot.$DATE.csv.gz"
-rm "links_snapshot.$DATE.csv.tmp"
+rm -f "links_snapshot.$DATE.csv.tmp"
 
-grep ".$DATE.csv.gz" "$input " | while read -r link_file; do
+firstline=true
+grep ".$DATE.csv.gz" "$input" | while read -r link_file; do
+  if $firstline; then
+    zcat "$link_file" | head -n1 >> "links_snapshot.$DATE.csv.tmp"
+    firstline=false
+  fi
+
+  if $verbose; then
     echo "zcat $link_file | tail -n+2 >> links_snapshot.$DATE.csv.tmp"
-    zcat "$link_file" | tail -n+2 >> "links_snapshot.$DATE.csv.tmp"
+  fi
+  zcat "$link_file" | tail -n+2 >> "links_snapshot.$DATE.csv.tmp"
 done
 
-sort -n -k1 "links_snapshot.$DATE.csv.tmp" | $output_compression > "link_snapshot.$DATE.csv.gz"
+sort -n -k1 "links_snapshot.$DATE.csv.tmp" | \
+  $output_compression > "link_snapshot.$DATE.csv.gz"
 rm "links_snapshot.$DATE.csv.tmp"
 
 exit 0
